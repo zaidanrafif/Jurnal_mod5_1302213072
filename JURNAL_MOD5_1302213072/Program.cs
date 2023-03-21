253f191dd3cdc8e57c1541598b081564a9b0cc0d
PC16-IFLAB <>
2023-03-21 16:15:01 +0700

    mod5_jurnal_main
    mod5_1302213072

---
+++ JURNAL_MOD5_1302213072/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[]args)
+    {
+        Penjumlahan angka = new Penjumlahan();
+        Console.WriteLine(angka.JumlahTigaAngka<float>(13, 02, 21));
+    }
+}
+
+
+
+public class Penjumlahan
+{
+    public T JumlahTigaAngka<T>(T input1, T input2, T input3)
+    {
+        dynamic angka1 = input1;
+        dynamic angka2 = input2;
+        dynamic angka3 = input3;
+        return angka1 + angka2 + angka3;
+
+    }
+}
